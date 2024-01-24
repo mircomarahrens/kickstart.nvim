138c856a200d9d5d33e42a5707ee9c790e2b0882
Mirco Marahrens <mirco.marahrens@googlemail.com>
2024-01-24 17:59:27 +0100

    fix lsp-zero (#1)

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,10 +1,4 @@
 return {
-    -- https://github.com/nvim-lua/plenary.nvim
-    -- All the lua functions I don't want to write twice.
-    {
-        "nvim-lua/plenary.nvim"
-    },
-
     -- https://github.com/nvim-telescope/telescope.nvim
     -- telescope.nvim is a highly extendable fuzzy finder over lists.
     {
@@ -193,6 +187,35 @@ return {
         end
     },
 
+    -- https://github.com/VonHeikemen/lsp-zero.nvim
+    -- Collection of functions that will help you setup Neovim's LSP client, so you can get IDE-like features with minimum effort.
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        dependencies = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
+        },
+        config = function()
+            local lsp = require('lsp-zero').preset({})
+
+            lsp.on_attach(function(client, bufnr)
+                -- see :help lsp-zero-keybindings
+                -- to learn the available actions
+                lsp.default_keymaps({buffer = bufnr})
+            end)
+
+            lsp.setup()
+        end
+    },
+
     -- https://github.com/williamboman/mason.nvim
     -- Mason is a plugin for Neovim that provides a simple way to manage your projects.
     {
@@ -207,64 +230,28 @@ return {
     {
         "williamboman/mason-lspconfig.nvim",
         config = function()
+            local lsp_zero = require("lsp-zero")
+            local lspconfig = require("lspconfig")
+
             require("mason-lspconfig").setup({
                 ensure_installed = {
-                    "lua_ls",
                     "clangd",
                     "cmake",
                     "pylsp",
-                    "tsserver",
                     "rust_analyzer"
                 },
+                handlers = {
+                    lsp_zero.default_setup,
+                    lua_ls = function()
+                        local lua_opts = lsp_zero.nvim_lua_ls()
+                        lspconfig.lua_ls.setup(lua_opts)
+                    end,
+               },
             })
         end
     },
 
-    -- https://github.com/VonHeikemen/lsp-zero.nvim
-    -- Collection of functions that will help you setup Neovim's LSP client, so you can get IDE-like features with minimum effort.
-    {
-        "VonHeikemen/lsp-zero.nvim",
-        branch = "v3.x",
-        config = function()
-            local lsp_zero = require("lsp-zero")
-            lsp_zero.extend_lspconfig()
-
-            lsp_zero.on_attach(function(client, bufnr)
-                local opts = {buffer = bufnr, remap = false}
-
-                vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-                vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-                vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-                vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-                vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-                vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-                vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-                vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-                vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-                vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-            end)
-        end
-    },
-    
-    -- https://github.com/neovim/nvim-lspconfig
-    -- Collection of configurations for built-in LSP client
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            local lsp_zero = require("lsp-zero")
-            lsp_zero.extend_lspconfig()
-
-            lspconfig.lua_ls.setup({})
-            lspconfig.clangd.setup({})
-            lspconfig.cmake.setup({})
-            lspconfig.pylsp.setup({})
-            lspconfig.tsserver.setup({})
-            lspconfig.rust_analyzer.setup({})
-        end
-    },
-
-    -- https://github.com/hrsh7th/cmp-nvim-lsp
+    -- https://github.com/hrsh7th/nvim-cmp
     -- nvim-cmp source for neovim builtin LSP client
     {
         "hrsh7th/nvim-cmp",
@@ -280,10 +267,12 @@ return {
         config = function()
             local cmp = require("cmp")
             local luasnip = require("luasnip")
+            local cmp_format = require("lsp-zero").cmp_format()
 
             require("luasnip/loaders/from_vscode").lazy_load()
 
             cmp.setup({
+                formatting = cmp_format,
                 completion = { completeopt = "menu,menuone,preview,noselect"},
                 -- configure how nvim-cmp interacts with the snippet engine
                 snippet = {
@@ -313,6 +302,133 @@ return {
             })
         end,
     },
+
+
+    --[[
+    {
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v3.x",
+    config = function()
+    local lsp_zero = require("lsp-zero")
+    lsp_zero.extend_lspconfig()
+
+    lsp_zero.on_attach(function(client, bufnr)
+    local opts = {buffer = bufnr, remap = false}
+
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    end)
+    end
+    },
+    --]]
+
+    --[[
+    -- https://github.com/williamboman/mason.nvim
+    -- Mason is a plugin for Neovim that provides a simple way to manage your projects.
+    {
+    "williamboman/mason.nvim",
+    config = function()
+    require("mason").setup({})
+    end
+    },
+
+    -- https://github.com/williamboman/mason-lspconfig.nvim
+    -- mason-lspconfig bridges mason.nvim with the lspconfig plugin
+    {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+    require("mason-lspconfig").setup({
+    ensure_installed = {
+    "clangd",
+    "cmake",
+    "pylsp",
+    "rust_analyzer"
+    },
+    })
+    end
+    },
+
+    -- https://github.com/neovim/nvim-lspconfig
+    -- Collection of configurations for built-in LSP client
+    {
+    "neovim/nvim-lspconfig",
+    config = function()
+    local lspconfig = require("lspconfig")
+    lspconfig.lua_ls.setup({
+    settings = {
+    Lua = {
+    diagnostics = {
+    -- Get the language server to recognize the `vim` global
+    globals = {'vim'},
+    },
+    },
+    },
+    })
+    lspconfig.clangd.setup({})
+    lspconfig.cmake.setup({})
+    lspconfig.pylsp.setup({})
+    lspconfig.rust_analyzer.setup({})
+    end
+    },
+
+    -- https://github.com/hrsh7th/nvim-cmp
+    -- nvim-cmp source for neovim builtin LSP client
+    {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-nvim-lsp",
+    "saadparwaiz1/cmp_luasnip",
+    "L3MON4D3/LuaSnip",
+    "rafamadriz/friendly-snippets",
+    },
+    config = function()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+
+    require("luasnip/loaders/from_vscode").lazy_load()
+
+    cmp.setup({
+    completion = { completeopt = "menu,menuone,preview,noselect"},
+    -- configure how nvim-cmp interacts with the snippet engine
+    snippet = {
+    expand = function(args)
+    luasnip.lsp_expand(args.body)
+    end,
+    },
+    mapping = {
+    ["<C-k>"] = cmp.mapping.select_prev_item(),
+    ["<C-j>"] = cmp.mapping.select_next_item(),
+    ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-e>"] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+    ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+    },
+    -- source for autocompletion
+    sources = {
+    -- snippet
+    { name = "luasnip" },
+    -- text within current buffer
+    { name = "buffer" },
+    -- file system paths
+    { name = "path" },
+    },
+    })
+    end,
+    },
+    --]]
 
     {
         "rust-lang/rust.vim"
