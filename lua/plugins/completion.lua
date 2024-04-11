ae3ee66256ca2ac6a774322e702cc6f2b00a3b60
Mirco Marahrens <mirco.marahrens@googlemail.com>
2024-04-11 17:07:14 +0200

    disable copilot per default

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -54,6 +54,7 @@ return {
     -- https://github.com/github/copilot.vim
     -- GitHub Copilot suggest code and entire functions in real-time right from your editor.
     {
-        "github/copilot.vim"
+        "github/copilot.vim",
+        enable = false,
     }
 }
--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -2,11 +2,11 @@ return {
     {
         -- https://github.com/rust-lang/rust.vim
         -- Rust support for Vim
-        "rust-lang/rust.vim"
+        "rust-lang/rust.vim",
     },
-    {
-        'mrcjkb/rustaceanvim',
-        version = '^4', -- Recommended
-        ft = { 'rust' },
-    }
+--    {
+--        'mrcjkb/rustaceanvim',
+--        version = '^4', -- Recommended
+--        ft = { 'rust' },
+--    }
 }
